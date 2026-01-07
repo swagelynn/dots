62ccf573c97c5bce1b1bb5e2f386b60dc35feb40
s4midev <astingraye@proton.me>
2026-01-07 23:29:46 +0000

    base foundation

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "meow... meowwwwwwwwww.... :3";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
